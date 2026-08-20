785096ca34d77801189ca58938d850ffee02573d
niv-347 <nivunkl347@gmail.com>
2026-08-20 23:40:42 +0700

    update api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,25 @@
 <?php
 
+// Buat folder temporary wajib Laravel di runtime Vercel
+$storageDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+// Arahkan path cache & view compiled Laravel ke /tmp
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+
 require __DIR__ . '/../public/index.php';
